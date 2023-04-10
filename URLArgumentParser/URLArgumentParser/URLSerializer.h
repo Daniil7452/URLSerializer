f644a4ed1f414cb2fa29a75951c63956585aef1e
Daniil7452 <130360048+Daniil7452@users.noreply.github.com>
2023-04-10 14:45:41 +0300

    URLSerializer
    URLSerializer

--- URLArgumentParser/URLArgumentParser/URLSerializer.h
+++ URLArgumentParser/URLArgumentParser/URLSerializer.h
@@ -12,14 +12,29 @@ class URLSerializer
 public:
 	URLSerializer(const char* url);
 
+	/// <summary>
+	/// вернет метод запроса
+	/// </summary>
 	HTTPMethod GetMethod();
 
+	/// <summary>
+	/// вернет URL
+	/// </summary>
 	const std::string& GetURL();
 
+	/// <summary>
+	/// ¬ернет хост
+	/// </summary>
 	const std::string& GetHost();
 
+	/// <summary>
+	/// вернет элементы пути запроса, если они содержатс€ в URL
+	/// </summary>
 	const std::string& GetPathParam(uint8_t paramNum);
 
+	/// <summary>
+	/// ¬ернет пары <аргумент, значение> запроса, если они содержатс€ в URL
+	/// </summary>
 	const std::map<std::string, std::string>& GetQueryStrings();
 
 private:
@@ -29,6 +44,9 @@ private:
 	std::vector<std::string> _PathParams;
 	std::map<std::string, std::string> _QueryStrings;
 
+	/// <summary>
+	/// –азобьет строку на подстроки по указанному разделителю
+	/// </summary>
 	static std::vector<std::string> Split(std::string& str, char del);
 
 
